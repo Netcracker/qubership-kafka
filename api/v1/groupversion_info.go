44914f52eb36413ee0df7cf0e3493146e8a4d9e1
paia0720 <pavel.iadrov@netcracker.com>
2025-03-17 19:50:10 +0500

    fix

--- api/v1/groupversion_info.go
+++ api/v1/groupversion_info.go
@@ -25,7 +25,7 @@ import (
 
 var (
 	// GroupVersion is group version used to register these objects
-	GroupVersion = schema.GroupVersion{Group: getEnv("API_GROUP", "qubership.org"), Version: "v1"}
+	GroupVersion = schema.GroupVersion{Group: "qubership.org", Version: "v1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
