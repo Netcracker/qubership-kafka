9a0d913ac0f1bd86482e4f6e51e9a39a500af4fc
fibu0125 <ph.bouzid03@gmail.com>
2026-02-14 18:49:18 +0500

    fix: fix pattern.

--- operator/controllers/provider/mirror_maker_provider.go
+++ operator/controllers/provider/mirror_maker_provider.go
@@ -191,6 +191,7 @@ func (mmrp MirrorMakerResourceProvider) createReplicationFlowConfig(sourceNames 
 
 	if !repeatedReplication {
 		topicPrefixes = append(topicPrefixes, ".*\\..*-internal")
+		topicPrefixes = append(topicPrefixes, ".*\\..*\\.internal")
 		topicPrefixes = append(topicPrefixes, "__.*")
 		blackList := fmt.Sprintf("%s = %s", TopicBlackList, strings.Join(topicPrefixes, ","))
 		replicationConfig = append(replicationConfig, blackList)
