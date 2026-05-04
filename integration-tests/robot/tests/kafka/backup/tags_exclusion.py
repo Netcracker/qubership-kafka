84525b0cc37478c5ab46df4296a33d6abe7a9a29
PavelYadrov <iadrovpaul@gmail.com>
2026-05-04 16:29:39 +0300

    feat: enhance tag exclusion logic in backup tests

--- integration-tests/robot/tests/kafka/backup/tags_exclusion.py
+++ integration-tests/robot/tests/kafka/backup/tags_exclusion.py
@@ -20,11 +20,15 @@ def check_that_parameters_are_presented(environ, *variable_names) -> bool:
 
 
 def get_excluded_tags(environ) -> list:
+    excluded_tags = []
     if not check_that_parameters_are_presented(environ,
                                                'BACKUP_DAEMON_HOST',
                                                'BACKUP_DAEMON_PORT'):
-        return ['backup']
+        excluded_tags.append('backup')
     if not check_that_parameters_are_presented(environ,
                                                'BACKUP_DAEMON_USER',
                                                'BACKUP_DAEMON_PASSWORD'):
-        return ['unauthorized_access']
+        excluded_tags.append('unauthorized_access')
+    if environ.get('S3_ENABLED') != 'true':
+        excluded_tags.append('backup_s3')
+    return excluded_tags
