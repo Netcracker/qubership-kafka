f348a312b3cda113f73093975f0473e1a074e71f
PavelYadrov <iadrovpaul@gmail.com>
2026-05-05 12:06:17 +0300

    feat: update CI concurrency settings and fix tag exclusion logic in backup tests

--- integration-tests/robot/tests/kafka/backup/tags_exclusion.py
+++ integration-tests/robot/tests/kafka/backup/tags_exclusion.py
@@ -31,5 +31,5 @@ def get_excluded_tags(environ) -> list:
         excluded_tags.append('unauthorized_access')
     if not check_that_parameters_are_presented(environ, 'S3_ENABLED') \
             or environ.get('S3_ENABLED') != 'true':
-        excluded_tags.append('backup_s3')
+        excluded_tags.append('backup_v2')
     return excluded_tags
