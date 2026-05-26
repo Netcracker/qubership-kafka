a2a4e1a4ec4e9c3f18c2bcd9e7018b491c42df9d
PavelYadrov <iadrovpaul@gmail.com>
2026-05-26 13:51:37 +0300

    chore: enhance Dockerfile and entrypoint script with new wrapper and permissions, update Helm configuration for consistent environment variable syntax

--- mirror-maker-monitoring/docker/entrypoint-wrapper.sh
+++ mirror-maker-monitoring/docker/entrypoint-wrapper.sh
@@ -13,4 +13,4 @@ sed -i "s|__PROMETHEUS_URLS__|${PROMETHEUS_URLS}|g" "$CONFIG_DST"
 echo "Rendered prometheus urls:"
 grep -n "urls =" "$CONFIG_DST"
 
-exec /entrypoint.sh "$@" --config "$CONFIG_DST"
+exec /entrypoint.sh "$@" --config "$CONFIG_DST" 
