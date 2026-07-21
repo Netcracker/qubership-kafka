c374792d3efaa934a59b9b9063ca32e603353b7f
saja0219 <sahana.s.jadav@netcracker.com>
2026-07-21 14:51:41 +0530

    fix: add proxy class

---
+++ mirror-maker/kafka-mm2-extension/src/main/java/com/netcracker/kafka/mirror/extension/HeaderFilter.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2024-2025 NetCracker Technology Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netcracker.kafka.mirror.extension;
+
+import java.util.Map;
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.connector.ConnectRecord;
+import org.apache.kafka.connect.transforms.Transformation;
+
+public class HeaderFilter<R extends ConnectRecord<R>> implements Transformation<R> {
+
+  private final org.qubership.kafka.mirror.extension.HeaderFilter<R> delegate =
+      new org.qubership.kafka.mirror.extension.HeaderFilter<>();
+
+  @Override
+  public void configure(Map<String, ?> configs) {
+    delegate.configure(configs);
+  }
+
+  @Override
+  public R apply(R record) {
+    return delegate.apply(record);
+  }
+
+  @Override
+  public ConfigDef config() {
+    return delegate.config();
+  }
+
+  @Override
+  public void close() {
+    delegate.close();
+  }
+}
